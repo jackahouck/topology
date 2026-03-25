8b919aefe854bad044051e4902e91e0a10a10bb4
Jack Houck <jackahouck@gmail.com>
2026-03-24 20:32:14 -0400

    Drew the circle and holes around sampled points

--- render.py
+++ render.py
@@ -5,7 +5,7 @@ from vietoris_rips import vietoris_rips
 from cech import cech_complex
 
 
-def render_complex(points, complex, title="Simplicial Complex"):
+def render_complex(points, complex, holes, title="Simplicial Complex"):
     pts3d = np.column_stack([points, np.zeros(len(points))])
 
     plotter = pv.Plotter()
@@ -41,17 +41,31 @@ def render_complex(points, complex, title="Simplicial Complex"):
         tet_mesh = pv.PolyData(pts3d, lines=tet_lines)
         plotter.add_mesh(tet_mesh, color="purple", line_width=3)
 
+    # draw unit disk boundary
+    circle = pv.Circle(radius=1.0)
+    plotter.add_mesh(circle, color="steelblue", opacity=0.1)
+
+    # draw holes
+    for c in holes.centers:
+        hole = pv.Circle(radius=holes.R)
+        hole.translate([c[0], c[1], 0], inplace=True)
+        plotter.add_mesh(hole, color="white", opacity=0.8)
+        hole_edge = pv.Circle(radius=holes.R)
+        hole_edge.translate([c[0], c[1], 0], inplace=True)
+        plotter.add_mesh(hole_edge, color="navy", opacity=1.0, style="wireframe")
+
     plotter.view_xy()
     plotter.show()
 
 
 if __name__ == "__main__":
-    holes = place_holes(N=5, R=0.1)
-    points = sample_points(M=50, holes=holes)
-    r = 0.2
+    holes = place_holes(N=7, R=0.1)
+    points = sample_points(M=30, holes=holes)
+    r = 0.1
 
+    print(len(points))
     # Uncomment for Cech or VR
-    #complex = vietoris_rips(points, r)
-    complex = cech_complex(points, r)
+    complex = vietoris_rips(points, r)
+    #complex = cech_complex(points, r)
 
-    render_complex(points, complex, title="Rered Complex")
+    render_complex(points, complex, holes, title="Cech Complex")
